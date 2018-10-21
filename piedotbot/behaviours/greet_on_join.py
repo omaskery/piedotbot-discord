ed87811ecabf89cc710c99f898fbbd2e22735a1b
Oliver Maskery <omaskery@googlemail.com>
2018-10-21 19:45:06 +0100

    fixed method not overriding parent
    classic python, a wrongly named method wasn't actually overloading
    the method it was supposed to be - silently failing to ever be
    called :) :) :) excellent.

--- piedotbot/behaviours/greet_on_join.py
+++ piedotbot/behaviours/greet_on_join.py
@@ -5,7 +5,7 @@ from . import base_behaviour
 
 class Behaviour(base_behaviour.Behaviour):
 
-    async def on_member_joined(self, client, member):
-        msg = 'Welcome to {server.name}, {member.mention}'.format(server=member.server, member=member)
+    async def on_member_join(self, client, member):
+        msg = 'Welcome to {server.name}, {member.mention}!'.format(server=member.server, member=member)
         await client.bot.send_message(member.server, msg)
 
