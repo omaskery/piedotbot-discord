a3cae47f1f1b3456fbdaeada70ceaeef596aa79a
Oliver Maskery <omaskery@googlemail.com>
2018-10-21 16:06:37 +0100

    simplified the dice rolling plugin
    dice rolling was stupidly complex because of dumb features,
    they're gone now, so now it's simple

--- piedotbot/behaviours/dice_roll.py
+++ piedotbot/behaviours/dice_roll.py
@@ -1,9 +1,11 @@
-import asyncio
-import datetime
+from collections import namedtuple
 import random
 import re
 
 from . import base_behaviour
+
+
+RollDescription = namedtuple("RollDescription", "dice sides")
 
 
 class Behaviour(base_behaviour.Behaviour):
@@ -13,91 +15,42 @@ class Behaviour(base_behaviour.Behaviour):
         self.allowed_channels = ["bot-shennanigans"]
 
     async def on_command(self, client, original_msg, relevant_content):
+        max_rolls = 40
+
         author = original_msg.author
+
+        roll = self.parse_command(relevant_content)
+
+        if roll is None:
+            return
+
+        if roll.dice > max_rolls:
+            await client.bot.send_message(original_msg.channel, f'{author.mention} no')
+            return
+
+        total = 0
+        rolls = []
+        for index in range(roll.dice):
+            roll_result = random.randint(1, roll.sides)
+            if roll.dice <= max_rolls:
+                rolls.append(roll_result)
+            total += roll_result
+
+        average = total / roll.dice
+        msg = f'{author.mention} rolled {rolls} for a total of {total} and average of {average}'
+        await client.bot.send_message(original_msg.channel, msg)
+
+    @staticmethod
+    def parse_command(relevant_content):
         words = relevant_content.split()
-        if len(words) > 1 and words[0].lower() == 'roll':
-            max_visible_dice = 40
-            sleep_interval = datetime.timedelta(seconds=3.0)
-            sleep_period = 0.01
-            max_calculation_time = datetime.timedelta(minutes=1)
-            min_progress_msg_time = datetime.timedelta(seconds=10)
+        if len(words) != 2 or words[0].lower() != 'roll':
+            return None
 
-            pattern = re.compile(r'(\d+)d(\d+)')
-            match = pattern.fullmatch(words[1])
-            if match is not None:
-                dice, sides = int(match.group(1)), int(match.group(2))
-                if dice > 100000:
-                    warning = '{author.mention} hold up, I need to go think about this...'.format(author=author)
-                    await client.bot.send_message(original_msg.channel, warning)
+        pattern = re.compile(r'(\d+)d(\d+)')
+        match = pattern.fullmatch(words[1])
+        if match is None:
+            return None
 
-                total = 0
-                rolls = []
-                given_eta = False
-                start_time = datetime.datetime.now()
-                next_interval = start_time + sleep_interval
-                given_up = False
-                for index in range(dice):
-                    roll_result = random.randint(1, sides)
-                    if dice <= max_visible_dice:
-                        rolls.append(roll_result)
-                    total += roll_result
+        dice, sides = int(match.group(1)), int(match.group(2))
 
-                    now = datetime.datetime.now()
-                    if now >= next_interval:
-                        next_interval += sleep_interval
-
-                        print("  calculating {}/{} ({}%)".format(
-                            index, dice, round(index / dice * 100, 3)
-                        ))
-
-                        if not given_eta:
-                            time_so_far = now - start_time
-                            time_per_dice = time_so_far.total_seconds() / index
-                            dice_to_go = int(dice - (index + 1))
-                            seconds_remaining = time_per_dice * dice_to_go
-                            intervals = (
-                                ('seconds', 1), ('minutes', 60.0), ('hours', 60.0), ('days', 24.0),
-                            )
-                            time_to_go = None
-                            unit_of_time_remaining = seconds_remaining
-                            for interval in intervals:
-                                interval_name, interval_divider = interval
-                                unit_of_time_remaining /= interval_divider
-                                try:
-                                    time_to_go = datetime.timedelta(**{interval_name: unit_of_time_remaining})
-                                    break
-                                except OverflowError:
-                                    time_to_go = None
-                            if time_to_go is not None:
-                                print("time to go:", time_to_go)
-                                progress_msg = "{author.mention}, yeah... it's looking like {roll} is gonna take another {remaining} to figure out...".format(
-                                    author=author,
-                                    roll=words[1],
-                                    remaining=time_to_go
-                                )
-                                if time_to_go >= max_calculation_time:
-                                    progress_msg += " and I can't be bothered to do that. I give up."
-                                    given_up = True
-                                if time_to_go > min_progress_msg_time:
-                                    await client.bot.send_message(original_msg.channel, progress_msg)
-                                if given_up:
-                                    break
-                            else:
-                                progress_msg = "{author.mention}, it's hard to even guess how long that will take, so I'm not gonna bother with that one. I give up.".format(
-                                    author=author
-                                )
-                                await client.bot.send_message(original_msg.channel, progress_msg)
-                                given_up = True
-                                break
-                            given_eta = True
-                        await asyncio.sleep(sleep_period)
-
-                if not given_up:
-                    average = total / dice
-                    msg = '{author.mention} rolled {rolls} for a total of {total} and average of {average}'.format(
-                        author=author,
-                        rolls="[{}]".format(", ".join(map(str, rolls))) if dice <= max_visible_dice else "{} dice".format(dice),
-                        total=total,
-                        average=average
-                    )
-                    await client.bot.send_message(original_msg.channel, msg)
+        return RollDescription(dice, sides)
